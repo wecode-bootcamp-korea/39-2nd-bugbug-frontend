5cfb15fa3ecd16c6354eb9a8bbce4cfd75372011
myp <myp880@gmail.com>
2022-11-28 19:07:02 +0900

    폴더위치수정

--- src/styles/GlobalStyle.js
+++ src/styles/GlobalStyle.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React from 'react'; // eslint-disable-line no-unused-vars
 import { createGlobalStyle } from 'styled-components';
 import reset from 'styled-reset';
 
