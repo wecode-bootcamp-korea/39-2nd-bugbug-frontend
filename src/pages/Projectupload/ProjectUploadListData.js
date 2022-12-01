549f95897727a92b32a77b4f3d719cdb050e97a1
myp <myp880@gmail.com>
2022-12-01 15:03:40 +0900

    [39기 박문영] Add : Project Upload 기능구현
    이미지 미리보기 기능추가 완료

--- src/pages/Projectupload/ProjectUploadListData.js
+++ src/pages/Projectupload/ProjectUploadListData.js
@@ -17,9 +17,11 @@ export const PROJECT_UPLAOD_LIST = [
     explan:
       '썸네일로 등록될 이미지 파일을 선택하신 후 업로드 버튼을 눌러주세요',
     type: 'file',
+    file: null,
   },
   {
     id: 4,
+    name: 'category',
     explan: '프로젝트 카테고리를 선택하세요.',
     type: 'radio',
     choice: [
@@ -41,13 +43,25 @@ export const PROJECT_UPLAOD_LIST = [
   },
   {
     id: 7,
-    name: 'date-start',
+    name: 'gift',
+    explan: '후원 신청 금액을 입력해주세요.',
+    type: 'number',
+  },
+  {
+    id: 8,
+    name: 'gift_information',
+    explan: '후원 설명을 작성해주세요',
+    type: 'text',
+  },
+  {
+    id: 9,
+    name: 'date_start',
     explan: '프로젝트 시작일을 정해주세요.',
     type: 'date',
   },
   {
-    id: 8,
-    name: 'date-end',
+    id: 10,
+    name: 'date_end',
     explan: '프로젝트 종료일을 정해주세요.',
     type: 'date',
   },
