5bba1d11e6f8173d0356188937b94616f4b6c56b
a young <xx.36.5MHz@gmail.com>
2022-12-01 13:27:18 +0900

    ADD : productdetail 페이지 레이아웃 구현

--- src/pages/Productdetail/Productdetail.js
+++ src/pages/Productdetail/Productdetail.js
@@ -1,5 +1,12 @@
 import React from 'react';
+import ProjectSummary from './ProjectSummary';
+import ProductInfo from './ProjectInfo';
 
 export default function Productdetail() {
-  return <div>Productdetail</div>;
+  return (
+    <>
+      <ProjectSummary />
+      <ProductInfo />
+    </>
+  );
 }
