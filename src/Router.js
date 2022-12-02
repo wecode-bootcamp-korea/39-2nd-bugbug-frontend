34c6ad141031d94c3139d477608c25f8f549b7f9
myp <myp880@gmail.com>
2022-12-02 16:18:14 +0900

    feature/router-settings conflict 해결

--- src/Router.js
+++ src/Router.js
@@ -1,7 +1,6 @@
 import React from 'react';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
-import Header from './components/Header/Header';
-import Footer from './components/Footer/Footer';
+import MainLayout from './routerLayout/MainLayout';
 import Main from './pages/Main/Main';
 import Signin from './pages/Signin/Signin';
 import Signup from './pages/Signup/Signup';
@@ -12,16 +11,16 @@ import Projectupload from './pages/Projectupload/Projectupload';
 export default function Router() {
   return (
     <BrowserRouter>
-      <Header />
       <Routes>
-        <Route path="/" element={<Main />} />
+        <Route element={<MainLayout />}>
+          <Route path="/" element={<Main />} />
+          <Route path="/productdetail" element={<Productdetail />} />
+          <Route path="/projectupload" element={<Projectupload />} />
+        </Route>
+        <Route path="/payment" element={<Payment />} />
         <Route path="/signin" element={<Signin />} />
         <Route path="/signup" element={<Signup />} />
-        <Route path="/productdetail/:id" element={<Productdetail />} />
-        <Route path="/payment" element={<Payment />} />
-        <Route path="/projectupload" element={<Projectupload />} />
       </Routes>
-      <Footer />
     </BrowserRouter>
   );
 }
